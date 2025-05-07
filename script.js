da380bc739a639bef9fa5d8c6a29b4d64e14078d
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-05-07 15:00:43 +0200

    Add basic IIFE gameboard

---
+++ script.js
@@ -0,0 +1,10 @@
+const gameBoard = (function () {
+    const board = [];
+
+    for (let i = 0; i < 3; i++) {
+        board[i] = [];
+        for (let j = 0; j < 3; j++) {
+            board[i].push("");
+        }
+    }
+})();
